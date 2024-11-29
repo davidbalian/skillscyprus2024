ca0f3c8e193f64f79c7b747ab86de019b6b12f4c
David <davidbalyan9@gmail.com>
2024-11-29 11:11:04 +0200

    handled result if only meal name is entered

--- scripts/getMeals.js
+++ scripts/getMeals.js
@@ -1,6 +1,7 @@
 const apiUrl = "https://www.themealdb.com/api/json/v1/1/";
 
 let form = document.getElementById("form");
+let searchResultsParentDiv = document.querySelector(".search-results-inner");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,10 +10,24 @@ form.addEventListener("submit", (e) => {
   let parsedFormData = Object.fromEntries(formData);
   console.log(parsedFormData);
 
+  // search by name if only meal is entered
   if (parsedFormData.mealName) {
     fetch(`${apiUrl}/search.php?s=${parsedFormData.mealName}`)
       .then((res) => res.json())
-      .then((data) => console.log(data.meals))
+      .then((data) => {
+        console.log(data.meals);
+
+        searchResultsParentDiv.innerHTML = "";
+
+        let meals = data.meals;
+
+        meals.forEach((meal) => {
+          let searchResult = document.createElement("div");
+          searchResult.className = "search-result-meal";
+          searchResult.innerHTML = meal.strMeal;
+          searchResultsParentDiv.appendChild(searchResult);
+        });
+      })
       .catch((error) => console.error("Errror: ", error));
   }
 });
